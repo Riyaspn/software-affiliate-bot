06299266d6b3f0196f599b147a2e6765043949fc
Riyaspn <111150254+Riyaspn@users.noreply.github.com>
2025-08-02 02:17:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,5 @@
-# config.py
+import os
 
-# Telegram bot credentials
-
-
-# Timing or behavior settings (if needed later)
-POST_DELAY = 2  # Seconds between messages
-
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+CHANNEL_ID = os.environ.get("CHANNEL_ID")
+POST_DELAY = int(os.environ.get("POST_DELAY", "2"))  # default 2 seconds if not set
